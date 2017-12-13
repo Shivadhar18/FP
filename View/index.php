17e9c377871fc5cf1df5938e2d668d39df9fd25b
Shivadhar18 <shivadharreddy@gmail.com>
2017-12-13 23:57:31 +0530

    Add Todo List Design

--- View/index.php
+++ View/index.php
@@ -1,23 +1,107 @@
 
 <?php require 'inc/header.php' ?>
+<style>
+button.accordion {
+    background-color: #fff;
+    color: #444;
+    cursor: pointer;
+    padding: 18px;
+    width: 100%;
+    border: none;
+    text-align: left;
+    outline: none;
+    font-size: 16px;
+    transition: 0.4s;
+}
 
-<?php /* if (empty($this->oPosts)): ?>
-    <p class="bold">There is no Blog Post.</p>
-    <p><button type="button" onclick="window.location='<?=ROOT_URL?>?p=blog&amp;a=add'" class="bold">Add Your First Blog Post!</button></p>
-<?php else: ?>
+button.accordion.active{
+    border-bottom: 1px solid #ddd;
 
-    <?php foreach ($this->oPosts as $oPost): ?>
-        <h1><a href="<?=ROOT_URL?>?p=blog&amp;a=post&amp;id=<?=$oPost->id?>"><?=htmlspecialchars($oPost->title)?></a></h1>
+}
+button.accordion .fa-trash{
+    float: right;
+}
 
-        <p><?=nl2br(htmlspecialchars(mb_strimwidth($oPost->body, 0, 100, '...')))?></p>
-        <p><a href="<?=ROOT_URL?>?p=blog&amp;a=post&amp;id=<?=$oPost->id?>">Want to see more?</a></p>
-        <p class="left small italic">Posted on <?=$oPost->createdDate?></p>
-
-        <?php require 'inc/control_buttons.php' ?>
-        <hr class="clear" /><br />
-    <?php endforeach ?>
-
-<?php endif */?>
-
-<div style="margin-top: 800px;bottom: 0px;"></div>
+div.panel {
+    background-color: white;
+    max-height: 0;
+    overflow: hidden;
+    transition: max-height 0.2s ease-out;
+}
+div.panel.panelactive{
+    border-bottom:1px solid #edf1f2;
+    padding: 15px 18px;
+}
+.text-delete{
+    text-decoration: line-through;
+}
+</style>
+<div class="row">
+    <div class="col-md-12">
+      <div class="grid simple">
+        <div class="grid-title no-border">
+            <div class="page-title"><i class="fa fa-tasks"></i>
+                    <h3>My <span class="semi-bold">Todo List</span></h3>
+            </div>
+            <div class="col-md-12 text-center">
+                <?php require 'inc/msg.php' ?>
+            </div>
+<?php
+$completed=0;
+$pending=0;
+ foreach ($this->oTodos as $oTodo): ?>
+    
+            <button class="accordion">
+                <span class="<?php if ($oTodo->status==1): echo 'text-delete'; $completed++; else: echo 'semi-bold'; $pending++; endif; ?> ">
+                        <?=$oTodo->title?>
+                </span> 
+                    <?php if ($oTodo->status!=1):?>
+                        <span class="fa fa-pencil edit-row" data-id="<?=$oTodo->id?>"></span> 
+                    <?php endif;?>
+                <span class="fa fa-trash delete-row" data-id="<?=$oTodo->id?>"></span>
+            </button>
+            
+            <div class="panel">
+                <p><?=$oTodo->body?></p>
+            </div>
+ <?php endforeach ?>
+            <div align="right">
+            <?=$completed;?> Completed- <?=$pending;?> Pending
+            </div>
+        </div>
+      </div>
+    </div>
+  </div>
 <?php require 'inc/footer.php' ?>
+<script>
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].onclick = function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+      panel.classList.toggle('panelactive');
+    if (panel.style.maxHeight){
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    } 
+  }
+}
+$(".delete-row").click(function(){
+    var did=$(this).attr('data-id');
+    var confim=confirm('Do You Want To Delete This Row?');
+    if(confim==true){
+        window.location.href="?p=todo&a=delete&did="+did;
+    }
+
+    return false;
+});
+$(".edit-row").click(function(){
+    var eid=$(this).attr('data-id');
+    window.location.href="?p=todo&a=todo&a=edit&eid="+eid;
+    return false;
+})
+</script>
+
