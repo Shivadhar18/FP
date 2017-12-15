ecabfb9f731a6e0a53647a45144b3259293f0153
Shivadhar18 <shivadharreddy@gmail.com>
2017-12-15 14:04:37 +0530

    SESSION Changes

--- View/inc/msg.php
+++ View/inc/msg.php
@@ -2,14 +2,18 @@
 //$_SESSION['sErrMsgcount']="";
 //$_SESSION['sSuccMsgcount']="";
 if(!isset($_SESSION['sErrMsgcount'])){
-	$_SESSION['sErrMsgcount']=0;
+	$_SESSION['sErrMsgcount']=2;
+	$_SESSION['sErrMsg']="";
 }
 if(!isset($_SESSION['sSuccMsgcount'])){
-	$_SESSION['sSuccMsgcount']=0;
+	$_SESSION['sSuccMsgcount']=2;
+	$_SESSION['sSuccMsg']="";
 }
+
+//print_r($_SESSION);
  if (!empty($_SESSION['sErrMsg'])): ?>
     <h5 class="label label-danger"><?=$_SESSION['sErrMsg']?></h5>
-<?php  if($_SESSION['sErrMsgcount']==2){ $_SESSION['sErrMsgcount']=1;  }else{ $_SESSION['sErrMsgcount']=2; $_SESSION['sErrMsg']=""; } endif ?>
+<?php  if($_SESSION['sErrMsgcount']==2){ $_SESSION['sErrMsgcount']=1;  }else{ $_SESSION['sErrMsgcount']=2; $_SESSION['sErrMsg']="";  } endif ?>
 
 <?php if (!empty($_SESSION['sSuccMsg'])): ?>
     <h5 class="label label-success"><?=$_SESSION['sSuccMsg']?></h5>
